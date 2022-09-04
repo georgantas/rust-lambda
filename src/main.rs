d7bbdf7b41e84280a55f0b6d67532e2962e79033
Georgantas <18753033+Georgantas@users.noreply.github.com>
2022-09-04 12:16:47 -0400

    init.

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use lambda_runtime::{service_fn, LambdaEvent, Error};
+use serde::{Serialize, Deserialize};
+use serde_json::{json, Value};
+
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let func = service_fn(func);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct NameInputEvent {
+  pub first_name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct HelloWorldResponse {
+  pub message: String,
+}
+
+async fn func(event: LambdaEvent<NameInputEvent>) -> Result<HelloWorldResponse, Error> {
+    let (event, _context) = event.into_parts();
+
+    let hello_world_response = HelloWorldResponse {
+        message: format!("Hello, {}!", event.first_name),
+    };
+
+    Ok(hello_world_response)
+}
